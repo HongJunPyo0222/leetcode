5ba454bb8dd8a9f0abab0c3e0deaa3eba78a9465
홍준표 <117992714+HongJunPyo0222@users.noreply.github.com>
2026-04-24 17:52:16 +0900

    Time: 23 ms (2.05%), Space: 12.6 MB (3.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,19 +5,17 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         q = []
-        lenk = len(matrix)
+
         for y, row in enumerate(matrix):
             for x, val in enumerate(row):
                 q.append([val, x, y])
         maxlen = len(matrix) - 1
-        print(q)
-        half = len(matrix)/ 2
+
+
         while q:
             node = q.pop()
             print(node)
             val, x, y = node
             matrix[x][maxlen - y] = val
 
-        for row in matrix:
-            print(row)
 
