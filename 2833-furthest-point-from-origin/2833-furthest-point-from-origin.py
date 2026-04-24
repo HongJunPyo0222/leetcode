e2a657e006e782d968f31a2162f64522eef3b07d
홍준표 <117992714+HongJunPyo0222@users.noreply.github.com>
2026-04-24 12:45:23 +0900

    Time: 0 ms (100%), Space: 12.4 MB (18.92%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def furthestDistanceFromOrigin(self, moves):
+        """
+        :type moves: str
+        :rtype: int
+        """
+
+        """
+
+        L, R, _로 구성된 스트링 , 이 스트링은 당신의 움직임을 나타냄 0 에서 시작
+
+        i 번째 움직임에서  moves[i] = l 혹은 _이면 l로 이동
+        origin에서 가장 먼 거리를 가야함
+        """
+
+
+        countL = 0
+        countR = 0
+        count_ = 0
+        cur = 0
+
+        for char in moves:
+            if char == "L":
+                cur -=1
+            elif(char == "R"):
+                cur +=1
+            else:
+                count_ +=1
+
+        
+        if cur < 0:
+            return abs(cur - count_)
+        else :
+            return cur + count_
