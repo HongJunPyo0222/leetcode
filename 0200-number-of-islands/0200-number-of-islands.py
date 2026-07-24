2b0a5adaa64ff07db0027b25a94abd9993deb259
홍준표 <117992714+HongJunPyo0222@users.noreply.github.com>
2026-07-24 11:44:07 +0900

    Time: 233 ms (44.79%), Space: 29.2 MB (27.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -21,24 +21,19 @@ class Solution(object):
 
         checked = [[False] * w for _ in range(h)]
 
-        for row in grid:
-            print(row)
-        print()
-        for row in checked:
-            print(row)
+
 
         def spread(x, y):
             q = deque()
             q.append([x, y])
             checked[y][x] =True
-            print(q)
             while(q):
                 x, y = q.popleft()
                 for dx, dy in zip(dxs, dys):
                     if can_move(x + dx, y + dy):
                         q.append([x + dx, y + dy])
                         checked[y + dy][x + dx] = True
-                        print("spread")
+
             
 
         for y in range(h):
@@ -48,7 +43,6 @@ class Solution(object):
 
                 if grid[y][x] == '0':
                     continue
-                print("x= ", x, "y = ",y)
                 spread(x, y)
                 answer +=1
 
